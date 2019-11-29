ea49626d72ab9f686d28c0c642eda305e11e23ad
Bertin Clement <Clement.Bertin@bordeaux-inp.fr>
2019-11-29 11:04:43 +0100

    revision creation de tables

--- CreationTable.sql
+++ CreationTable.sql
@@ -1,5 +1,5 @@
 -- -- ============================================================
--- --   Nom de la base   :  CINEMA
+-- --   Nom de la base   :  Cartes
 -- --   Nom de SGBD      :  ORACLE Version 7.0
 -- --   Date de creation :  30/10/96  12:09
 -- -- ============================================================
@@ -16,101 +16,86 @@
 
 create table PARTIE(
   IdPartie INT PRIMARY KEY NOT NULL,
-  Lieu varchar(100), -- Taille max lieu ?
+  Lieu varchar(100),
   Date DATE,
-  TypeTournoi INT -- Est-ce un int ?
+  TypeTournoi varchar(100)
 );
 
-create table AVOIR_JOUE(
-  #IdPartie INT PRIMARY KEY,
-  #IdJoueur INT PRIMARY KEY,
+create table JEU( -- A verifier
   foreign key (#IdPartie) references PARTIE(IdPartie),
-  foreign key (#IdJoueur) references JOUEUR(IdJoueur),
-  Adversaire varchar(100), -- Taille max nom ?
-  Resultat Int -- Pas certain que ça soit un int
+  foreign key (#IdJoueur1) references JOUEUR(IdJoueur),
+  foreign key (#IdJoueur2) references JOUEUR(IdJoueur),
+  IdVictoire Int
 );
 
-create table ETRE_UTILISE(
-  #IdPartie INT PRIMARY KEY,
-  #IdJoueur INT PRIMARY KEY,
+create table UTILISATIONS( -- A verifier
   foreign key (#IdPartie) references PARTIE(IdPartie),
   foreign key (#IdJoueur) references JOUEUR(IdJoueur),
 );
 
-create table JOUEUR(
+create table JOUEURS(
   IdJoueur INT PRIMARY KEY,
-  Nom varchar(100), -- Taille max nom ?
-  Prenom varchar(100), -- Taille max nom ?
-  Pseudonyme(100), -- Taille max nom ?
+  Nom varchar(100),
+  Prenom varchar(100),
+  Pseudonyme varchar(100)
 );
 
-create table J_POSSEDER_E( -- Nom Ok ?
-  #IdJoueur INT PRIMARY KEY,
-  #IdExemplaire INT PRIMARY KEY,
+create table POSSESSIONS_EXEMPLAIRES( -- A vérifier
   foreign key (#IdJoueur) references JOUEUR(IdJoueur),
   foreign key (#IdExemplaire) references EXEMPLAIRE(IdExemplaire),
 );
 
-create table EXEMPLAIRE( -- NOTE : J'ai enlevé le Nom Edition car est déjà dans edition
+create table EXEMPLAIRES(
   IdExemplaire INT PRIMARY KEY,
   DateAcquisition DATE,
-  ModeAcquisition INT, -- Pas certain que ça soit un int
   DatePerte DATE,
+  ModeAcquisition varchar(100),
   Qualite INT,
-  Effet varchar(20), -- Taille max effet ?
-  #IdEdition INT,
-  #IdCarte INT,
+  Effet varchar(100),
   foreign key (#IdEdition) references EDITION(IdEdition),
   foreign key (#IdCarte) references CARTE(IdCarte)
 );
 
-create table EDITION(
+create table EDITIONS(
   IdEdition INT PRIMARY KEY,
-  NomEdition varchar(100) -- Taille max nom ?
+  NomEdition varchar(100)
 );
 
-create table APPARTENIR(
-  #IdEdition INT PRIMARY KEY,
-  #IdCarte INT PRIMARY KEY,
+create table APPARTENANCES(
   foreign key (#IdEdition) references EDITION(IdEdition),
   foreign key (#IdCarte) references CARTE(IdCarte)
   NbTirages INT,
-  Carte varchar(100) -- Taille max carte ?
+  Carte varchar(100)
 );
 
-create table CARTE(
+create table CARTES(
   IdCarte PRIMARY KEY,
-  Titre varchar(100), -- Taille max titre ?
-  DescCarte varchar(1000), -- Taille max desc ? + pas le même nom
-  Type varchar(20), -- Taille max type ?
-  Nature varchar(20), -- Taille max nature ?
+  Titre varchar(100),
+  DescCarte varchar(1000),
+  Type varchar(20),
+  Nature varchar(20),
   Niveau INT
 );
 
-create table DECK(
+create table DECKS(
   IdDeck INT PRIMARY KEY,
-  #IdJoueur INT,
   foreign key (#IdJoueur) references JOUEUR(IdJoueur),
-  NomDeck varchar(100) -- Taille max nom ?
+  NomDeck varchar(100)
 );
 
-create table CONTENIR(
-  #IdDeck INT PRIMARY KEY,
-  #IdCarte INT PRIMARY KEY,
+create table CONTENANCE(
   foreign key (#IdDeck) references DECK(IdDeck),
   foreign key (#IdCarte) references CARTE(IdCarte),
   NbCartes INT
 );
 
-create table C_POSSEDER_C( -- nom ?
-  #IdCarte INT PRIMARY KEY,
-  #IdCaracteristique INT PRIMARY KEY,
+create table POSSESSIONS_CARACTERISTIQUES( -- On en a besoin ?
   foreign key (#IdCarte) references CARTE(IdCarte),
   foreign key (#IdCaracteristique) references CARACTERISTIQUE(IdCaracteristique)
 );
 
 create table CARACTERISTIQUE(
   IdCaracteristique INT PRIMARY KEY,
-  DescCara varchar(1000), -- Taille max desc ? + pas le même nom
+  DescCaracteristique varchar(1000),
   Valeurs INT
 );
