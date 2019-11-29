45acb696e392e6eb972193f307abc8e47594a456
Bertin Clement <Clement.Bertin@bordeaux-inp.fr>
2019-11-29 11:21:43 +0100

    ajout des requètes de consultation

---
+++ Consultation.sql
@@ -0,0 +1,13 @@
+create view List_type as
+  (select * from CARTES
+  where CARTES.type = 'Magie'); -- A chenger en fonctiond de ce que veut recherché l'utilisateur
+
+create view List_no_deck as
+  (select * from Cartes
+  minus
+  (select CONTENANCE.IdCarte from Contenance));
+
+create view list_collectionneur as
+  (select * from JOUEURS
+  minus
+  (select UTILISATIONS.IdJoeur from UTILISATION));
