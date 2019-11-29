e42bc889816b34d280a2512fb09a570368365c5a
Bertin Clement <Clement.Bertin@bordeaux-inp.fr>
2019-11-29 11:35:23 +0100

    consultation 2.0

--- Consultation.sql
+++ Consultation.sql
@@ -8,14 +8,14 @@ end;
 
 create view List_type as
   (select * from CARTES
-  where CARTES.type = typeUtilisateur); -- A chenger en fonctiond de ce que veut recherché l'utilisateur
+  where CARTES.TYPE_CARTE = typeUtilisateur); -- A chenger en fonctiond de ce que veut recherché l'utilisateur
 
 create view List_no_deck as
   (select * from Cartes
   minus
-  (select CONTENANCE.IdCarte from Contenance));
+  (select CONTENANCE.ID_CARTE from Contenance));
 
 create view list_collectionneur as
   (select * from JOUEURS
   minus
-  (select UTILISATIONS.IdJoeur from UTILISATION));
+  (select UTILISATIONS.ID_JOUEUR from UTILISATION));
