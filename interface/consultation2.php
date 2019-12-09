6d32a388654db2aa23f72ab1713eba1635e20f04
Pierre Chevallier <pchevalli001@enseirb-matmeca.fr>
2019-12-09 10:19:03 +0100

    Interface bdd

---
+++ interface/consultation2.php
@@ -0,0 +1,35 @@
+<html>
+  <head>
+    <title>Consultation 2</title>
+    <meta charset="utf-8" />
+  </head>
+
+  <?php
+  include("menu.php");
+  try{
+    //Connexion à MySql
+    $bdd = new PDO('mysql:host=localhost;dbname=cartes_a_collectionner;charset=utf8', 'root', 'Kaae7vad');
+  }catch (Exception $e){
+    die('Erreur : ' . $e->getMessage());
+  }
+  ?>
+
+  <body>
+    <h2>Liste des cartes qui ne font partie d'aucun deck</h2>
+
+    <?php
+    $req = $bdd->query('select * from CARTES
+    where CARTES.ID_CARTE not in
+    (select ID_CARTE from CONTENANCE)');
+
+    while($donnees = $req->fetch()){?>
+      <li><strong><?php echo $donnees['TITRE'];?></strong>
+      , de nature <strong><?php echo $donnees['NATURE_CARTE']?></strong></br> 
+      <?php echo $donnees['DESC_CARTE']; ?></br></br></li>
+
+      <?php
+      }
+      $req->closeCursor(); //Fin de traitement
+      ?>
+  </body>
+</html>
---
+++ interface/menu.php
@@ -0,0 +1,11 @@
+<nav id="menu">
+    <div class="element_menu">
+        <h3>Menu des requêtes SGBD</h3>
+        <ul>
+          <li><a href="index.php">Page d'Accueil</a></li>
+          <li><a href="consultation1.php">Consultation 1 : Liste des cartes d'un certain type</a></li>
+          <li><a href="consultation2.php">Consultation 2 : Liste des cartes qui ne font partie d'aucun deck</a></li>
+          <li><a href="consultation3.php">Consultation 3 : Liste des joueurs collectionneurs</a></li>
+        </ul>
+    </div>
+</nav>
