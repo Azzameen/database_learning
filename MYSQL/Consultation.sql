5da9935379ea48b64ac02bb94565c355db3dc6b1
Corentin Le Métayer <clemetayer@enseirb-matmeca.fr>
2019-12-10 21:57:53 +0100

    updated statistique/classement

--- MYSQL/Consultation.sql
+++ MYSQL/Consultation.sql
@@ -1,21 +1,13 @@
--- declare
---   typeUtilisateur varchar(100);
---
--- begin
---   set typeUtilisateur = $1;
--- end;
--- /
-
 create view Liste_type_carte as
   (select * from CARTES
   where TYPE_CARTE = 'Monstre normal');
 
-create view Liste_sans_deck as -- NOTE : changé le premier select * en select ID_CARTE (sinon fonctionne pas)
+create view Liste_sans_deck as
   (select ID_CARTE from CARTES
   where CARTES.ID_CARTE not in
   (select ID_CARTE from CONTENANCE));
 
-create view Liste_collectionneur as -- NOTE : idem
+create view Liste_collectionneur as
   (select ID_JOUEUR from JOUEURS
   where JOUEURS.ID_JOUEUR not in
   (select ID_JOUEUR from UTILISATIONS));
